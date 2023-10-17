1c578dc27136e537166e5e5cf59cc98d131a800d
Janek1010 <janekdomozych99@gmail.com>
2023-10-17 16:15:29 +0200

    creating testRestTemplate with basic configuration

---
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
@@ -0,0 +1,71 @@
+package guru.springframework.spring6resttemplate.client;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
+@RestClientTest(BeerClientImpl.class)
+class BeerClientMockTest {
+
+    static final String URL = "http://localhost:8080";
+
+    @Autowired
+    BeerClient beerClient;
+
+    @Autowired
+    MockRestServiceServer server;
+
+    // Spring Boot automatycznie tworzy instancje objectmappera wiec chill
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testListBeers() throws JsonProcessingException {
+        // to co mock zwraca
+        String payLoad = objectMapper.writeValueAsString(getPage());
+
+        // konfiguracja interakcji z serwerem
+        server.expect(method(HttpMethod.GET))
+                .andExpect(requestTo(URL +  BeerClientImpl.GET_BEER_PATH))
+                .andRespond(withSuccess(payLoad, MediaType.APPLICATION_JSON));
+
+
+        Page<BeerDTO> dtos = beerClient.listBeers();
+        assertThat(dtos.getContent().size()).isGreaterThan(0);
+
+    }
+
+    BeerDTO getBeerDto() {
+        return BeerDTO.builder()
+                .id(UUID.randomUUID())
+                .price(new BigDecimal("10.99"))
+                .beerName("Mango Bobs")
+                .beerStyle(BeerStyle.IPA)
+                .quantityOnHand(500)
+                .upc("12345")
+                .build();
+    }
+
+    BeerDTOPageImpl getPage() {
+        return new BeerDTOPageImpl(Arrays.asList(getBeerDto()), 1, 25, 1);
+    }
+}
